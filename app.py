8f257ecd0238ae82f32638f8a1afe708e652fd96
Khushmita Kaur <khushp9894@gmail.com>
2024-02-24 18:13:01 -0800

    dynamic routing

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World'
-    
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/<name>')
+def print_name(name):
+    return 'Hi, {}'.format(name)
